8276b509a04c54cedc3ddb72ea284566bf8d1930
6545 FAIZAN AHMED <fazitanveer2@gmail.com>
2023-02-18 16:32:09 +0500

    modules

---
+++ pymodules.py
@@ -0,0 +1,10 @@
+def index():
+    print("I am Faizan!")
+
+
+class myclass():
+    def __init__(self, message):
+        self.message = message
+
+    def report(self):
+        print(self.message)
